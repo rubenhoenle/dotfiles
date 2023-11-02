182da9600bfde822969c0facc15b7db6f8b525ff
Ruben Hoenle <git@hoenle.xyz>
2023-11-02 21:05:19 +0100

    feat(nix): add new ssh key to agenix

--- secrets/README.md
+++ secrets/README.md
@@ -2,3 +2,12 @@
 AWS_ACCESS_KEY_ID=<MY_ACCESS_KEY>
 AWS_SECRET_ACCESS_KEY=<MY_SECRET_ACCESS_KEY>
 ```
+
+## Adding a new key to agenix
+Add the new public key into `secrets.nix`.
+
+``` bash
+# rekey the secrets
+agenix -r
+```
+
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,8 @@
 let
   deathstar = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEgK7/NOff+EL/e2MQ8e2ELoU2nFMsytxqpDyKaQhE7N";
+  millenium-falcon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPSGQkg+lmetJAVsd0Ojy76ehuoc2aJuIP03f08Ny0lQ";
 in
 {
-  "restic-password.age".publicKeys = [ deathstar ];
-  "backblaze-b2-restic-s3-secrets.age".publicKeys = [ deathstar ];
+  "restic-password.age".publicKeys = [ deathstar millenium-falcon ];
+  "backblaze-b2-restic-s3-secrets.age".publicKeys = [ deathstar millenium-falcon ];
 }
