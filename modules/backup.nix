7ab9883362a4409d2f78837ea50e18f5082e6ae9
Ruben Hoenle <git@hoenle.xyz>
2024-04-05 23:33:53 +0200

    chore(nixos): use pre-defined restic backup service
    instead of the self-defined systemd restic backup services

--- modules/backup.nix
+++ modules/backup.nix
@@ -2,6 +2,36 @@
 with lib;
 let
   cfg = config.ruben.backup;
+  localRepo = "/run/media/ruben/SAMSUNG/restic-nixos";
+  resticPasswordFile = config.age.secrets.resticPassword.path;
+  excludeFile = pkgs.writeText "restic-excludes.txt"
+    ''
+      /home/ruben/.bash_history
+      /home/ruben/.bash_profile
+      /home/ruben/.bashrc
+      /home/ruben/.cache
+      /home/ruben/.config
+      /home/ruben/.docker
+      /home/ruben/.gnupg
+      /home/ruben/.local
+      /home/ruben/.mozilla
+      /home/ruben/.nix-defexpr
+      /home/ruben/.nix-profile
+      /home/ruben/.pki
+      /home/ruben/.profile
+      /home/ruben/.vim
+      /home/ruben/.viminfo
+      /home/ruben/.vscode
+      /home/ruben/.zshenv
+      /home/ruben/.zsh_history
+      /home/ruben/.zshrc
+      node_modules
+      __pycache__
+      /home/ruben/nobackup
+      !/home/ruben/.mozilla/firefox/**/bookmarkbackups
+      !/home/ruben/.local/share/Trash
+      !/home/ruben/.local/share/PrismLauncher
+    '';
 in
 {
   options.ruben.backup = {
@@ -10,122 +40,36 @@ in
 
   config = mkIf (cfg.enable)
     {
-      systemd.user.services =
-        let
-          s3DefaultRegion = "eu-central-003";
-          remoteRepo = "s3:https://s3.eu-central-003.backblazeb2.com/nixos-restic-backup";
-          s3SecretsEnvironmentFile = config.age.secrets.backblazeB2ResticS3EnvironmentSecrets.path;
-          localRepo = "/run/media/ruben/SAMSUNG/restic-nixos";
-          resticPasswordFile = config.age.secrets.resticPassword.path;
-          backupPaths = "/home/ruben";
-          excludeFile = pkgs.writeText "restic-excludes.txt"
-            ''
-              /home/ruben/.bash_history
-              /home/ruben/.bash_profile
-              /home/ruben/.bashrc
-              /home/ruben/.cache
-              /home/ruben/.config
-              /home/ruben/.docker
-              /home/ruben/.gnupg
-              /home/ruben/.local
-              /home/ruben/.mozilla
-              /home/ruben/.nix-defexpr
-              /home/ruben/.nix-profile
-              /home/ruben/.pki
-              /home/ruben/.profile
-              /home/ruben/.vim
-              /home/ruben/.viminfo
-              /home/ruben/.vscode
-              /home/ruben/.zshenv
-              /home/ruben/.zsh_history
-              /home/ruben/.zshrc
-              node_modules
-              __pycache__
-              /home/ruben/nobackup
-              !/home/ruben/.mozilla/firefox/**/bookmarkbackups
-              !/home/ruben/.local/share/Trash
-              !/home/ruben/.local/share/PrismLauncher
-            '';
-          backupExcludes = "--exclude-caches --exclude-file=${excludeFile}";
-          keep = {
-            hourly = "48";
-            daily = "7";
-            weekly = "4";
-            monthly = "6";
-            yearly = "5";
-          };
-        in
-        {
-          restic_init = {
-            serviceConfig = {
-              Type = "oneshot";
-              EnvironmentFile = "${s3SecretsEnvironmentFile}";
-              ExecStart = "${pkgs.restic}/bin/restic init -r ${remoteRepo} -o s3.region=${s3DefaultRegion} --password-file ${resticPasswordFile}";
-            };
-            path = [
-              pkgs.openssh
-            ];
-          };
-          restic_backup = {
-            serviceConfig = {
-              Type = "oneshot";
-              EnvironmentFile = "${s3SecretsEnvironmentFile}";
-              ExecStart = "${pkgs.restic}/bin/restic backup -r ${remoteRepo} ${backupExcludes} -o s3.region=${s3DefaultRegion} --password-file ${resticPasswordFile} --one-file-system --tag systemd.timer ${backupPaths}";
-              ExecStartPost = "${pkgs.restic}/bin/restic forget -r ${remoteRepo} -o s3.region=${s3DefaultRegion} --password-file ${resticPasswordFile} --tag systemd.timer --group-by \"paths,tags\" --keep-hourly ${keep.hourly} --keep-daily ${keep.daily} --keep-weekly ${keep.weekly} --keep-monthly ${keep.monthly} --keep-yearly ${keep.yearly}";
-            };
-            onFailure = [ "restic_unlock.service" ];
-            path = [
-              pkgs.openssh
-            ];
-          };
-          restic_backup_to_harddrive = {
-            # systemctl list-units --type=mount
-            after = [ "run-media-ruben-SAMSUNG.mount" ];
-            requires = [ "run-media-ruben-SAMSUNG.mount" ];
-            wantedBy = [ "run-media-ruben-SAMSUNG.mount" ];
-            serviceConfig = {
-              ExecStart = "${pkgs.restic}/bin/restic backup -r ${localRepo} ${backupExcludes} --password-file ${resticPasswordFile} --one-file-system --tag systemd.timer ${backupPaths}";
-            };
-          };
-          restic_prune = {
-            serviceConfig = {
-              Type = "oneshot";
-              EnvironmentFile = "${s3SecretsEnvironmentFile}";
-              ExecStart = "${pkgs.restic}/bin/restic -r ${remoteRepo} -o s3.region=${s3DefaultRegion} --password-file ${resticPasswordFile} prune";
-            };
-            path = [
-              pkgs.openssh
-            ];
-          };
-          restic_unlock = {
-            serviceConfig = {
-              Type = "oneshot";
-              EnvironmentFile = "${s3SecretsEnvironmentFile}";
-              ExecStart = "${pkgs.restic}/bin/restic -r ${remoteRepo} -o s3.region=${s3DefaultRegion} --password-file ${resticPasswordFile} unlock";
-            };
-            unitConfig = {
-              OnSuccess = "restic_backup.service";
-            };
-            path = [
-              pkgs.openssh
-            ];
-          };
+      services.restic.backups.backblaze = {
+        user = "ruben";
+        initialize = true;
+        passwordFile = config.age.secrets.resticPassword.path;
+        repository = "s3:https://s3.eu-central-003.backblazeb2.com/nixos-restic-backup";
+        environmentFile = config.age.secrets.backblazeB2ResticS3EnvironmentSecrets.path;
+        paths = [ "/home/ruben" ];
+        pruneOpts = [
+          "--keep-hourly 48"
+          "--keep-daily 7"
+          "--keep-weekly 4"
+          "--keep-monthly 12"
+          "--keep-yearly 5"
+        ];
+        extraOptions = [ "s3.region=eu-central-003" ];
+        extraBackupArgs = [ "--exclude-caches" "--exclude-file=${excludeFile}" ];
+        timerConfig = {
+          OnCalendar = "hourly";
+          Persistent = true;
         };
-      systemd.user.timers = {
-        restic_backup = {
-          wantedBy = [ "timers.target" ];
-          partOf = [ "restic_backup.service" ];
-          timerConfig = {
-            OnCalendar = "hourly";
-            Persistent = true;
-          };
-        };
-        restic_prune = {
-          wantedBy = [ "timers.target" ];
-          partOf = [ "restic_prune.service" ];
-          timerConfig = {
-            OnCalendar = "monthly";
-            Persistent = true;
+      };
+
+      systemd.user.services = {
+        restic_backup_to_harddrive = {
+          # systemctl list-units --type=mount
+          after = [ "run-media-ruben-SAMSUNG.mount" ];
+          requires = [ "run-media-ruben-SAMSUNG.mount" ];
+          wantedBy = [ "run-media-ruben-SAMSUNG.mount" ];
+          serviceConfig = {
+            ExecStart = "${pkgs.restic}/bin/restic backup -r ${localRepo} --exclude-caches --excludeFile=${excludeFile} --password-file ${resticPasswordFile} --one-file-system --tag systemd.timer /home/ruben";
           };
         };
       };
