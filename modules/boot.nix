2766e27c96b56363c4b72ac3678a5b8f0882d45e
Ruben Hoenle <git@hoenle.xyz>
2024-06-09 21:18:50 +0200

    feat(nixos): enable aarch64-linux emulation

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,5 +1,10 @@
+{ config, ... }:
 {
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  # needed for https://github.com/rubenhoenle/pi-nixos
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+  nix.settings.extra-platforms = config.boot.binfmt.emulatedSystems;
 }
