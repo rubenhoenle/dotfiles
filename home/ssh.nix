70e27e7658d53309d27b75d660d95577cb16b07d
Ruben Hoenle <git@hoenle.xyz>
2024-06-22 19:04:18 +0200

    feat(ssh): add scarif to defined ssh hosts

--- home/ssh.nix
+++ home/ssh.nix
@@ -15,8 +15,18 @@
       mandalore-nixos-initrd = {
         host = "mandalore-initrd";
         hostname = "192.168.178.5";
-        port = 69;
+        port = 2222;
         user = "root";
+        identityFile = [
+          "~/.ssh/id_ed25519_sk"
+          "~/.ssh/yubikey-5-usb-c/id_ed25519_sk.pub"
+        ];
+      };
+      scarif-nixos = {
+        host = "scarif";
+        hostname = "192.168.178.4";
+        port = 69;
+        user = "ruben";
         identityFile = [
           "~/.ssh/id_ed25519_sk"
           "~/.ssh/yubikey-5-usb-c/id_ed25519_sk.pub"
