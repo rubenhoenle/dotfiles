0540ea9462c4172abac16c0c4aba290aef03f5b1
Ruben Hoenle <git@hoenle.xyz>
2024-07-05 10:29:32 +0200

    feat(vscode): wrap file tabs in vs code

--- home/home.nix
+++ home/home.nix
@@ -59,6 +59,10 @@
       github.vscode-github-actions
       sonarsource.sonarlint-vscode
     ];
+    userSettings = {
+      "terminal.integrated.gpuAcceleration" = "off";
+      "workbench.editor.wrapTabs" = true;
+    };
   };
 
 
