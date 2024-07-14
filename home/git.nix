8cbae0311a8fa113a18abfe2f792a98844dcec88
Ruben Hoenle <git@hoenle.xyz>
2024-07-14 17:14:34 +0200

    chore(nixos): flake update

--- home/git.nix
+++ home/git.nix
@@ -3,7 +3,6 @@ let
   cfg = osConfig.ruben.git;
   brancherer = pkgs.writeShellApplication {
     name = "branch";
-    runtimeInputs = [ pkgs.openssh ];
     text = ''
       ${pkgs.git}/bin/git branch | grep -v "^\*" | ${pkgs.fzf}/bin/fzf --height=20% --reverse --info=inline | xargs ${pkgs.git}/bin/git checkout
     '';
