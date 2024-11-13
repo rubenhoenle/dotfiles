4ff2e57a1a2b10daf1df32b6093a209405512e7b
Ruben Hoenle <git@hoenle.xyz>
2024-11-13 01:48:50 +0100

    chore(firewall): close open ports used for minecraft servers back then

--- modules/network.nix
+++ modules/network.nix
@@ -13,14 +13,7 @@ in
     networking = {
       hostName = cfg.hostname;
       networkmanager.enable = true;
-      firewall = {
-        enable = true;
-        allowedTCPPorts = [ 25565 ];
-        allowedUDPPorts = [ 25565 ];
-      };
-      hosts = {
-        "192.168.178.2" = [ "synology" "synology.fritz.box" ];
-      };
+      firewall.enable = true;
     };
 
     networking.networkmanager.ensureProfiles.profiles = {
