5359f874a9889d9f76212b4707e8ee4f9f4f9519
Ruben Hoenle <git@hoenle.xyz>
2024-11-16 15:01:57 +0100

    chore(age): add new key for millenium falcon

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,10 @@
 let
-  millenium-falcon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPSGQkg+lmetJAVsd0Ojy76ehuoc2aJuIP03f08Ny0lQ";
+  deathstar = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPSGQkg+lmetJAVsd0Ojy76ehuoc2aJuIP03f08Ny0lQ";
+  millenium-falcon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFQ1M3a2Vm5ihxBOayp6QznV0WwIVDTKyeFTenm/rxS1";
 in
 {
-  "restic-password.age".publicKeys = [ millenium-falcon ];
-  "backblaze-b2-restic-s3-secrets.age".publicKeys = [ millenium-falcon ];
-  "wireguard-private-key.age".publicKeys = [ millenium-falcon ];
-  "wireguard-preshared-key.age".publicKeys = [ millenium-falcon ];
+  "restic-password.age".publicKeys = [ deathstar ];
+  "backblaze-b2-restic-s3-secrets.age".publicKeys = [ deathstar ];
+  "wireguard-private-key.age".publicKeys = [ millenium-falcon deathstar ];
+  "wireguard-preshared-key.age".publicKeys = [ millenium-falcon deathstar ];
 }
